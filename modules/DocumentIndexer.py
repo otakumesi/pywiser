e9e3c68297fb1180e9d63d49ccb5b917840e56c7
Takuro Niitsuma <takuro-n@h28is016.naist.jp>
2019-10-28 23:23:09 +0900

    add indexer and searcher

---
+++ modules/DocumentIndexer.py
@@ -0,0 +1,12 @@
+class DocumentIndexer:
+    def __init__(self, db_path):
+        pass
+
+    def load_wikipedia_dump(self, file_path, max_index_count):
+        pass
+
+    def add_document(self, title, body):
+        if not (title or body):
+            return
+        pass
+
